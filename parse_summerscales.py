0afdc7f2458b3d27f33dc3014aa78350bc883f16
bwallace <byron.wallace@gmail.com>
2015-11-05 17:09:25 -0600

    fixd that god awful mess

--- parse_summerscales.py
+++ parse_summerscales.py
@@ -4,62 +4,54 @@ import pdb
 import numpy as np  
 
 import nltk 
-
 import BeautifulSoup as bs 
 
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def _just_the_txt(s):
+    return " ".join(s.findAll(text=True)).strip()
+
+##
+# fix this. a potential solution!!!
+# http://stackoverflow.com/questions/2957013/beautifulsoup-just-get-inside-of-a-tag-no-matter-how-many-enclosing-tags-there
 def get_tokens_and_lbls(annotated_data_path="summerscales-annotated-abstracts"): 
     pmids, docs, lbls = [], [], []
 
     for f in _get_xml_file_names(annotated_data_path):
-        print f
         soup = bs.BeautifulSoup(open(f).read())
         pmid = soup.find("abstract")['id']
-        pmids.append(pmid)
-
         sentences = list(soup.findAll("s"))
+        
         ordered_sentences = []
+        ordered_sentences_clean = []
         sentence_ids = [int(sent["id"]) for sent in sentences]
         d = dict(zip(sentence_ids, sentences))
+
         for s_id in sorted(d.keys()):
-            ordered_sentences.append(d[s_id])
+            cur_s = d[s_id]
+            # including tags
+            ordered_sentences.append(cur_s)
+            # stripped of tags; just the text
+            ordered_sentences_clean.append(_just_the_txt(cur_s))
 
         doc_sentence_tokens, doc_sentence_lbls = [], []
 
-        for sent in ordered_sentences:
+        for sent_idx, sent in enumerate(ordered_sentences):
             cleaned_sent, group_tokenized, sent_tokenized = [None]*3 # for sanity
+            cleaned_sent = ordered_sentences_clean[sent_idx]
+            sent_tokenized = nltk.word_tokenize(cleaned_sent)
+                   
+            # initialize label vector for the current sentence to -1s
+            sent_lbls = [-1 for _ in sent_tokenized]
 
-            group_str_in_sent = sent.find("group")
-            if group_str_in_sent is None: 
-                sent_tokenized = nltk.word_tokenize(sent.text)
-                sent_lbls = [-1 for _ in sent_tokenized]
-            else: 
-                # then this contains at least one group mention!
-                sent_lbls = []
-                group_text = sent.group.text
-
-                # now we have to deal with a small annoyance;
-                # the 'short' tags end up removing spaces
-                # from the text in the group tags, e.g.:
-                #   u'home  basedmedication  review'
-                # instead of 
-                #   u'home  based medication  review'
-                # here we fix that.
-                short_name = sent.short
-                if short_name is not None: 
-                    group_text = group_text.replace(short_name.text, " %s " % short_name.text)
-                    # also need to fix it up in the whole sentence!
-                    cleaned_sent = sent.text.replace(short_name.text, " %s " % short_name.text)
-
-                else: 
-                    cleaned_sent = sent.text 
-
-                cleaned_sent = cleaned_sent.replace(group_text, " %s " % group_text)
+            group_strs_in_sent = sent.findAll("group")
+            group_texts = []
+            for group_str in group_strs_in_sent:
+                group_text = _just_the_txt(group_str) #group_str.text
 
                 group_tokenized = nltk.word_tokenize(group_text)
-                sent_tokenized  = nltk.word_tokenize(cleaned_sent)
                 
-                # initialize label vector for the current sentence to -1s
-                sent_lbls = [-1 for _ in sent_tokenized]
                 # and now flip on labels corresponding to the tokens in the
                 # group span we read out
                 try:
@@ -68,16 +60,30 @@ def get_tokens_and_lbls(annotated_data_path="summerscales-annotated-abstracts"):
                     pdb.set_trace()
                 group_len = len(group_tokenized)
                 sent_lbls[start_index:start_index+group_len] = [1]*group_len
+            
+        doc_sentence_lbls.append(sent_lbls)
+        doc_sentence_tokens.append(sent_tokenized)
 
-                
-            doc_sentence_lbls.append(sent_lbls)
-            doc_sentence_tokens.append(sent_tokenized)
 
         docs.extend(doc_sentence_tokens)
         lbls.extend(doc_sentence_lbls)
+        pmids.extend([pmid]*len(doc_sentence_lbls))
 
-    return pmids, docs, lbls
-        #docs.append(all_sentences)
+    
+    ## just use sklearn to get indices for vocab
+    V = []
+    for d in docs:
+        V.extend(d)
+
+    # will probably blow up if you introduce more data... 
+    vocab = list(set(V))
+    v = CountVectorizer(ngram_range=(1,1), vocabulary=vocab, binary=True)
+    texts = [" ".join(doc) for doc in docs]
+    v.fit([" ".join(doc) for doc in docs])
+
+    return pmids, docs, lbls, v
+
+
 
 
 def _get_xml_file_names(dir_):
