5377d812f0881205ddffb6e4f8f773985521f85a
bwallace <byron.wallace@gmail.com>
2015-11-19 17:50:19 -0600

    (more) working on distant tagging of interventions in abstracts

--- distant_intervention_tag.py
+++ distant_intervention_tag.py
@@ -18,7 +18,7 @@ stopwords = stopwords.words('english')
 # some of these are just words that are likely to be shared
 # between cdsr text and abstract, even though not describing
 # interventions specifically
-stopwords.extend(["either", "time", "patients"])
+stopwords.extend(["either", "time", "patients", "or"])
 
 def annotate():
     bviewer = BiViewer()
@@ -116,6 +116,45 @@ def _clean_tokens(tokens):
             cleaned.append(t)
     return cleaned 
     
+def _match_and_tag(iv_tokens, abstract_tokens, top_k=1):
+    # match longest substring
+    matched, spans = longest_common_substrings(
+                        abstract_tokens, 
+                        iv_tokens, return_spans=True)
+    
+    matched_lens = [len(_clean_tokens(match)) for match in matched]
+    if len(matched_lens) == 0:
+        max_lens = []
+    else:
+        max_lens = [len_ for len_ in sorted(matched_lens, reverse=True)[:top_k] 
+                        if len_ > 0]
+
+    if len(max_lens) == 0:
+        # no matches.
+        return []
+    else:
+        longest_indices = [idx for idx, x in enumerate(matched_lens) if x in max_lens] #matched_lens.index(max_len)
+        best_matches, tagged_spans = [], []
+        for idx in longest_indices:
+            tagged_spans.append((" ".join(matched[idx]), spans[idx][0], spans[idx][1]))
+          
+    return tagged_spans
+
+
+def _tag_general_nondrug(study):
+    '''
+    @TODO there is a lot of overlap with _tag_vs 
+    to be refactored!
+    '''
+    intervention_text = _iv_for_study(study)
+    abstract = _abstract_for_study(study)
+
+    iv_tokens = nltk.word_tokenize(intervention_text.lower())
+    abstract_tokens = nltk.word_tokenize(abstract.lower())
+    
+    tagged_spans = _match_and_tag(iv_tokens, abstract_tokens, top_k=2)
+
+    return tagged_spans
 
 def _tag_vs(study):
     intervention_text = _iv_for_study(study)
@@ -127,9 +166,9 @@ def _tag_vs(study):
         iv_tokens = nltk.word_tokenize(iv.lower())
         abstract_tokens = nltk.word_tokenize(abstract.lower())
         # match longest substring
-        matched = longest_common_substrings(
+        matched, spans = longest_common_substrings(
                             abstract_tokens, 
-                            iv_tokens)
+                            iv_tokens, return_spans=True)
         
         matched_lens = [len(_clean_tokens(match)) for match in matched]
         if len(matched_lens) == 0:
@@ -142,13 +181,15 @@ def _tag_vs(study):
             return []
         else:
             longest_indices = [idx for idx, x in enumerate(matched_lens) if x == max_len] #matched_lens.index(max_len)
-            best_matches = []
+            best_matches, tagged_spans = [], []
             for idx in longest_indices:
                 if matched[idx] not in best_matches:
                     best_matches.append(matched[idx])
-            pdb.set_trace()
-            
-            ### TODO actually 'tag' (i.e., put the index spans in!)
+                    
+
+                tagged_spans.append((" ".join(matched[idx]), spans[idx][0], spans[idx][1]))
+                
+    return tagged_spans
 
 def _tag_tokens(tokens_list, t):
     indices = []
@@ -164,7 +205,7 @@ def tag_versus_studies(versus_studies):
             pass 
 
 def ds_interventions_abstracts(bviewer, num_studies=None):
-    tagged_pmids, tagged_abstracts, abstracts = [], [], []
+    tagged_pmids, tagged_abstracts, tagged_iv_texts, abstracts = [], [], [], []
     count = 0
     for study in bviewer:
         if count > num_studies:
@@ -181,26 +222,33 @@ def ds_interventions_abstracts(bviewer, num_studies=None):
         if intervention_text is None or abstract_text == "":
             pass
         else: 
-            if "vs" in intervention_text.split(" "):
-                if not _is_drug_trial(study):
-                    tagged_abstracts.append(_tag_vs(study))
-            #elif not _is_drug_trial(study):
-            #    pass 
+            tagged = None
+            if "vs" in intervention_text.split(" ") and not _is_drug_trial(study):
+                #tagged_abstracts.append(_tag_vs(study))
+                tagged = _tag_vs(study)
+            elif not _is_drug_trial(study):
+                # this is non-drug trial and 
+                tagged = _tag_general_nondrug(study)
             else:
                 # drug study!
                 tagged = _tag_drugs(study)
-                tagged_abstracts.append(tagged)
-                
-            abstracts.append(abstract_text)
-            tagged_pmids.append(cur_pmid)
-            count += 1
 
+            
+            if len(tagged) > 0:
+                tagged_abstracts.append(tagged)
+                abstracts.append(abstract_text)
+                tagged_pmids.append(cur_pmid)
+                tagged_iv_texts.append(intervention_text)
+                count += 1
+
+    pdb.set_trace()
     return tagged_pmids, tagged_abstracts
 
-def longest_common_substrings(s1, s2):
+def longest_common_substrings(s1, s2, return_spans=False):
     m = [[0] * (1 + len(s2)) for i in xrange(1 + len(s1))]
     longest, x_longest = 0, 0
     longest_list = []
+    spans = []
     for x in xrange(1, 1 + len(s1)):
         for y in xrange(1, 1 + len(s2)):
             if s1[x - 1] == s2[y - 1]:
@@ -209,7 +257,11 @@ def longest_common_substrings(s1, s2):
                     longest = m[x][y]
                     x_longest = x
                     longest_list.append(s1[x_longest - longest: x_longest])
+                    spans.append((x_longest - longest, x_longest))
             else:
                 m[x][y] = 0
+
+    if return_spans:
+        return longest_list, spans
     return longest_list#s1[x_longest - longest: x_longest]
 
