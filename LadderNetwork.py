a3ab3b9281efec0d36ee071b56464748766e759e
Eric Rincon <eric.rincon147@gmail.com>
2016-04-22 21:41:33 -0500

    Getting there...

--- LadderNetwork.py
+++ LadderNetwork.py
@@ -3,9 +3,12 @@ import tensorflow as tf
 # An implementation of a Ladder Network
 # Paper link: http://arxiv.org/pdf/1507.02672v2.pdf
 
-def batch_norm(x, h):
+def batch_norm(x):
     mean, variance = tf.nn.moments(x, axes=[0])
 
+    x_hat = (x - mean)/ tf.sqrt(variance)
+
+    return x_hat
 
 
 
@@ -22,16 +25,27 @@ class LadderNetworkMLP():
         y = tf.placeholder(tf.float32, shape=[None, output_size])
 
         # Parameters to learn for batch normalization
+        # Beta is the scaling parameter
+        # Gamma is the bias
+        # These parameters are redundant for linear activation functions
+        # such as ReLU.
+        # From page 6
+
         beta = tf.Variable()
         gamma = tf.Variable()
 
 
         # Create the layers for the network
 
+
+
         # Create the first layer
 
         W = tf.Variable(tf.zeros([input_size, layers_sizes[0]]))
         b = tf.Variable(tf.zeros(layers_sizes[0]))
+
+        # z_tilda is the preactivation of each layer
+        z_tilda = tf.Variable(tf.zeros()) + tf.random_normal()
 
         first_layer_mat = tf.matmul(X, W) + b
 
@@ -39,6 +53,8 @@ class LadderNetworkMLP():
         self.layers.append(first_layer_activation)
 
         for layer_i, layer_size in enumerate(layers_sizes):
+
+
             if layer_i == 0:
                 W = tf.Variable(tf.zeros([first_layer_activation, layer_size]))
                 b = tf.Variable([layer_size])
@@ -55,10 +71,14 @@ class LadderNetworkMLP():
             # Calculate current layer
             layer_mat_mul = tf.matmul(self.layers[layer_i - 1], W) + b
 
-            # Batch normalization
+            # Batch normalization and add isotropic gaussian noise
+            # From page 5, Algorithm 1
 
-            mean, variance = tf.nn.moments(layer_mat_mul)
+            batch_normalized_output = batch_norm(layer_mat_mul)
+            z_tilda = batch_normalized_output + tf.random_normal(batch_normalized_output.getshape())
 
+            # Activation at each layer
+            h = activation(tf.mul(gamma, (z_tilda + beta)))
 
 
             layer_activation = tf.nn.relu(layer_mat_mul)
@@ -70,6 +90,7 @@ class LadderNetworkMLP():
             layer_weights['z'] = layer_activation
 
             self.layers.append(layer_weights)
+
 
         W = tf.Variable(tf.zeros([output_size]))
         b = tf.Variable(tf.zeros([output_size]))
@@ -84,6 +105,45 @@ class LadderNetworkMLP():
         self.layers.append(output_layer_weights)
 
         output = tf.nn.softmax(output_mat_mul)
+
+
+        # Clean encoder (for denoising targets)
+        # See Algorithm 1
+        h_0 = X
+
+
+        for i, layer in enumerate(self.layers):
+            layer_weights
+            W = layer['W']
+            b = layer['b']
+
+            if i == 0:
+                h = h_0
+            else:
+                h = self.layers[i-1]
+
+            # Calculate the preactivation for the layer z
+
+            z = tf.matmul(W, h)
+
+            # mu
+            # sigma
+            # z_norm
+            # h activation
+
+
+
+        for layer_i, layer in enumerate(self.layers):
+            if layer_i == 1:
+                u = batch_norm(h)
+            else:
+                u = batch_norm()
+
+
+
+        # Decoder and denoising
+
+        for
 
         return output
 
